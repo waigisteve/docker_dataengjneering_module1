68ee26baadc8005e63d65039c41992161c39923f
waigisteve <120468516+waigisteve@users.noreply.github.com>
2026-01-23 20:40:07 +0000

    committing simple docker pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+
+df = pd.DataFrame({"day": [1, 2], "num_trips": [3, 4]})
+month = sys.argv[2] if len(sys.argv) > 2 else "01"
+df['month'] = month
+print(df.head())
+
+day = sys.argv[1] if len(sys.argv) > 1 else "01"
+df.to_parquet(f"output_day_{day}.parquet")
